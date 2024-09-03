52ebdc448454db156a7ab8e9d9c74e0613bfaba2
frostAdarsh <adarshmani017@gmail.com>
2024-09-03 11:35:30 +0530

    var

--- JS/index.js
+++ JS/index.js
@@ -1,2 +1,22 @@
 // console.log("Hello world")
 
+//----------------------------------------------------------
+
+// intor to variables
+
+// variables can store some information 
+// we can use that information later 
+// we can change that information later
+
+// declare a variable
+
+// var firstName = "Adarsh"
+
+// use a variable
+
+// console.log(firstName)
+
+// Change value
+
+// firstName = "Frost";
+// console.log(firstName)
