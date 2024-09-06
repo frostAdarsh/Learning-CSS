b53ea133dd66c57669cc92b7074bf94b43f42183
frostAdarsh <adarshmani017@gmail.com>
2024-09-06 17:19:40 +0530

    loop for

--- JS/index.js
+++ JS/index.js
@@ -149,3 +149,14 @@
 // console.log(array1)
 // console.log(array2)
 
+//-----------------------------------------------------------------
+//for loop in array
+
+// let fruits = ["apple","banana","grapes","mango"];
+// let fruits2 = []
+// for(let i=0;i<fruits.length;i++){
+//   fruits2.push(fruits[i].toUpperCase())
+// }
+// console.log(fruits2);
+
+//-----------------------------------------------------------
