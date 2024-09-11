12042ec4d785bd62e315c85a20139740a45905c3
frostAdarsh <adarshmani017@gmail.com>
2024-09-11 14:09:35 +0530

    map method

--- JS/index.js
+++ JS/index.js
@@ -264,5 +264,30 @@
 // })
 
 //----------------------------------------------------
+//map method
+
+// const numbers = [3,4,6,1,8];
+// const square = function(number){
+//   return number*number;
+// }
+// we can use arrow  function also 
+// const squareNumber = numbers.map(function(number){
+//   return number*number
+// })
+// console.log(squareNumber)
+
+// const users =[
+//      {firstName:"harshit",age:23},
+//      {firstName:"mohit",age:21},
+//      {firstName:"nitish",age:22},
+//      {firstName:"garima",age:20},
+//    ]
+
+// const userNames = users.map((user)=>{
+//   return user.firstName;
+// })
+// console.log(userNames);
+
+//------------------------------------------------------------
 
 
