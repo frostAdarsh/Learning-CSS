b8964d603787aa594431448d77822999468e264c
frostAdarsh <adarshmani017@gmail.com>
2024-09-11 14:47:52 +0530

    reduce

--- JS/index.js
+++ JS/index.js
@@ -301,3 +301,23 @@
 //   return number % 2 !== 0;
 // });
 // console.log(oddNumbers);
+
+//----------------------------------------------------------
+//reduce
+
+// const numbers = [1,2,3,4,5,10];
+
+// const sum = numbers.reduce((accumulator,currentValue)=>{
+//   return accumulator + currentValue;
+// },0);
+// console.log(sum)
+
+// const userCart = [
+//   { productId: 1, productName: "mobile", price: 12000 },
+//   { productId: 2, productName: "mobile", price: 14000 },
+//   { productId: 3, productName: "mobile", price: 20000 },
+// ];
+// const totalAmount = userCart.reduce((totaPrice, currentValue) => {
+//   return totaPrice + currentValue.price;
+// }, 0);
+// console.log(totalAmount);
