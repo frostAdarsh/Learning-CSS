1373f204f98926e9edf75119128be4d330317a79
frostAdarsh <adarshmani017@gmail.com>
2024-09-14 11:48:30 +0530

    Sets

--- JS/index.js
+++ JS/index.js
@@ -321,3 +321,41 @@
 //   return totaPrice + currentValue.price;
 // }, 0);
 // console.log(totalAmount);
+
+
+//-----------------------------------------------------
+//Set----------------------------------------------
+
+// Set (it is iterable)
+// store Data
+// sets also have its own methods
+// No index-based access 
+// Order is not guranteed 
+// unique items only (no duplicates allowed) 
+
+// const items = ["item1",'item2','item3'];
+// const numbers = new Set();
+// numbers.add(1);
+// numbers.add(2);
+// numbers.add(3);
+// numbers.add(4);
+// numbers.add(5);
+// numbers.add(6);
+// numbers.add(items)
+// if(numbers.has(1)){
+//     console.log('present')
+// }else{
+//     console.log('not present')
+// }
+// console.log(numbers)
+// for(let number of numbers){
+//     console.log(number)
+// }
+
+// const myArray = [1,2,3,4,4,5,6,5,6];
+// const uniqueElement = new Set(myArray);
+// let length = 0;
+// for(let element of uniqueElement){
+//     length++;
+// }
+// console.log(length)
